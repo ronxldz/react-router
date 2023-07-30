6653be15070b1d9e72ef4717a4eae48db981ad74
ronxldz <erik.p.1208@icloud.com>
2023-07-30 18:35:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div id="container">
+      <h1></h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">
+      <Routes>
+        <Route path="/blue" element={<h1>Blue</h1>} />
+        <Route path="/red" element={<h1>Red</h1>} />
+      </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
