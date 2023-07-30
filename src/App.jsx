a5cf2fe08780d70783c27dbe591125a50edcebd5
ronxldz <erik.p.1208@icloud.com>
2023-07-30 19:11:57 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/blue'
+import Red from './components/red'
+import { Home } from './components/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div id="container">
       <h1></h1>
-      <div id="navbar">{/* navigation here */}</div>
+      <div id="navbar">
+        <Link to="/">
+          Home
+        </Link>
+        <Link to="/blue">
+          Blue
+        </Link>
+        <Link to="/red">
+          Red
+        </Link>
+      </div>
       <div id="main-section">
       <Routes>
-        <Route path="/blue" element={<h1>Blue</h1>} />
-        <Route path="/red" element={<h1>Red</h1>} />
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/blue" element={<Blue/>} />
+        <Route path="/red" element={<Red/>} />
       </Routes>
       </div>
     </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export function Home() {
+    
+    return (
+        <div className="Home">
+            <h1>Home</h1>
+        </div>
+    );
+}
